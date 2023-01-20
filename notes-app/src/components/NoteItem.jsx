53c843f5dfa3ddcfb5b9c1e77895122f99ec8a62
Mesh Njihia <meshgath@gmail.com>
2023-01-20 05:49:49 +0300

    fix date overflow

--- notes-app/src/components/NoteItem.jsx
+++ notes-app/src/components/NoteItem.jsx
@@ -6,13 +6,13 @@ const NoteItem = ({ note }) => {
     //   note
     <Link
       to={`/edit-note/${note.id}`}
-      className="note flex text-base bg-teal-300 hover:opacity-90 p-4 flex-col transition-all duration-300 ease-out text-clr-white"
+      className="note flex text-base bg-teal-300 hover:opacity-90 pt-4 px-4 pb-[0.2rem] flex-col transition-all duration-300 ease-out text-clr-white justify-between"
     >
       <h4 className='font-medium font-dmsans'>{note.title.length > 35 ? (note.title.substr(0, 35)) + '...' : note.title}</h4>
       {/* <p className='text-xs'>
         {note.details.length >100 ? (note.details.substr(0,100)) +'...' : note.details}
       </p> */}
-          <p className='opacity-85 text-[0.52rem] text-slate-900 font-montserrat font-thin whitespace-nowrap bg-white px-2.5'>
+          <p className='opacity-85 text-[0.52rem] text-slate-900/75 font-montserrat font-thin whitespace-wrap leading-none bg-stone-300/30 pb-0 px-2.5 max-w-[10rem] text-center'>
               {note.date}
           </p>
     </Link>
